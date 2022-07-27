91a013121d7179688c4f4ecd9d46fbf5b7a9c6b6
Lera2323 <vassinalera@gmail.com>
2022-07-27 10:40:58 +0300

    решили задачу 25 на возведение числа в степень 2 числа, создали метод

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace ConsolApp
+{
+    class Program
+    {
+        static void Main (string[]args)
+        {
+            //Задача 25: Напишите цикл, который принимает на вход два натуральных числа (A и B) и возводит число A в степень B
+         
+         int number = 3;
+         int power = 5;
+         int result = number;
+         int count = 1;
+
+         while (count <= power)
+         {
+             result *= number;
+             count++;
+         }
+         Console.WriteLine ($"Число {number} в степени {power} = {result}");
+        }
+    }
+}
